fabb7e8ba3abefb7edf737b96690a83aef1dd64c
ChoEun <short0720@gmail.com>
2022-11-14 04:20:50 +0900

    6주차 챌린지

---
+++ 6thclass_3/app/src/main/java/com/example/a6thclass_3/PlayFragment.kt
@@ -0,0 +1,29 @@
+package com.example.a6thclass_3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.a6thclass_3.databinding.FragmentPlayBinding
+
+
+class PlayFragment : Fragment() {
+
+//    private val mPager : ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return FragmentPlayBinding.inflate(layoutInflater).root
+    }
+}
